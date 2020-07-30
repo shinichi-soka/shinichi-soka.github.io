e8d10493f3fa172ed3389565a0f8d507de82334a
shinichi-soka <68991196+shinichi-soka@users.noreply.github.com>
2020-07-30 20:42:13 +0900

    Add files via upload

---
+++ kimatu.js
@@ -0,0 +1,98 @@
+$('#detail-elephant').click(function(){
+
+    $('#elephant').slideToggle();
+
+});
+
+$('#detail-lion').click(function(){
+
+    $('#lion').slideToggle();
+
+});
+
+$('#detail-tiger').click(function(){
+
+    $('#tiger').slideToggle();
+
+});
+
+$('#detail-zebra').click(function(){
+
+    $('#zebra').slideToggle();
+
+});
+
+$('#detail-giraffe').click(function(){
+
+    $('#giraffe').slideToggle();
+
+});
+
+$('#detail-chimpanzee').click(function(){
+
+    $('#chimpanzee').slideToggle();
+
+});
+
+$('#detail-koala').click(function(){
+
+    $('#koala').slideToggle();
+
+});
+
+$('#detail-kangaroo').click(function(){
+
+    $('#kangaroo').slideToggle();
+
+});
+
+$('#pageguide').click(function(){
+
+    $('#areaguide').slideToggle(1000);
+});
+
+
+
+$('#thesunrieshack').click(function(){
+
+    $('#coffeemenu').slideToggle();
+
+});
+
+$('#thesunrieshack2').click(function(){
+
+    $('#smoothemenu').slideToggle();
+
+});
+
+$('#thesunrieshack3').click(function(){
+
+    $('#Fresh Juice').slideToggle();
+
+});
+
+$('#thesunrieshack4').click(function(){
+
+    $('#tea').slideToggle();
+
+});
+
+$('#starbucks1').click(function(){
+
+    $('#scoffee').slideToggle();
+
+});
+
+$('#starbucks2').click(function(){
+
+    $('#sfrape').slideToggle();
+
+});
+
+$('#starbucks3').click(function(){
+
+    $('#stea').slideToggle();
+
+});
+
+
